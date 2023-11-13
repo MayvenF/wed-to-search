e9fcd0b6ec628fca1f34b3e9a9d050e43e908c8f
Maeven Fanebust <maeven.fanebust@gmail.com>
2023-11-13 14:52:34 +0200

    modified main.go with new functionality for v1.1.0

--- main.go
+++ main.go
@@ -4,9 +4,13 @@ import "fmt"
 
 func main(){
 	fmt.Println("Are you my mother?")
-	v100()
+	v110()
 }
 
 func v100(){
 	fmt.Println("I'm from v1.0.0")
+}
+
+func v110(){
+	fmt.Println("I'm from v1.1.0")
 }
